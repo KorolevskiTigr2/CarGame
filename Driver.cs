4b7251b960da999fbaa8c369bbf3ad2a43dc3d03
Alexander Medved <mega.medved87@gmail.com>
2023-08-03 23:21:53 -0500

    Add: Time.deltaTime

--- Driver.cs
+++ Driver.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
 
-  [SerializeField] float steerSpeed = 1f;
-  [SerializeField] float moveSpeed = 0.01f;
+  [SerializeField] float steerSpeed = 100f;
+  [SerializeField] float moveSpeed = 100f;
 
     void Start()
     {
@@ -15,8 +15,8 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-      float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
-      float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+      float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+      float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
       transform.Rotate(0,0,-steerAmount);
       transform.Translate(0,moveAmount,0);
     }
